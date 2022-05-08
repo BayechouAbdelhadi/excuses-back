fb0f8cd086e9bb99d28c2c3b09e606e5873cda51
BAYECHOU Abdelhadi <abdelhadi.bayechou@buawei.com>
2022-05-08 15:08:21 +0200

    initilize application

---
+++ src/test/java/com/symbolit/excuses/ExcusesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.symbolit.excuses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcusesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
