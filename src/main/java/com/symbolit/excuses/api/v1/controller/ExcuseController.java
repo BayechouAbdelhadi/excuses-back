0d9ca4e25bdc769f9495ce0bf0e8669a9d9ee392
BAYECHOU Abdelhadi <abdelhadi.bayechou@buawei.com>
2022-05-09 03:40:39 +0200

    add cross origin rules

--- src/main/java/com/symbolit/excuses/api/v1/controller/ExcuseController.java
+++ src/main/java/com/symbolit/excuses/api/v1/controller/ExcuseController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import io.swagger.annotations.ApiOperation;
 import com.symbolit.excuses.api.v1.model.entity.Excuse;
 import com.symbolit.excuses.api.v1.service.ExcuseService;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @Controller
 @RequestMapping("/api/v1/excuses")
